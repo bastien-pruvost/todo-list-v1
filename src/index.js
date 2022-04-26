cfba3a3b8f227d151670d54892240485a966a450
Bastien Pruvost <bastien.mory@gmail.com>
2022-04-26 18:21:16 +0200

    feat : Delete Todo & Toggle editmode

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const localStorageTodoData = localStorage.getItem('todoList');
 const todoData = JSON.parse(localStorageTodoData) || [];
 const todoListContainer = document.querySelector('.todo-list');
 const newTaskForm = document.querySelector('.todo-add');
+const newTaskInput = document.querySelector('.todo-add__text-input');
 
 const displayTodoList = () => {
   todoListContainer.innerHTML = '';
   todoData.map((task, index) => {
-    createTaskElement(task, index);
+    task.editMode ? createTaskElementEditMode(task, index) : createTaskElement(task, index);
   });
 };
 
@@ -16,24 +17,58 @@ const createTaskElement = (task, index) => {
   const liTemplate = document.querySelector('#task-template');
   const liClone = document.importNode(liTemplate.content, true);
   const toggleFinish = liClone.querySelector('.task__toggle-finish');
+  const taskText = liClone.querySelector('.task__text');
+  const editButton = liClone.querySelector('.task__edit-btn');
   const deleteButton = liClone.querySelector('.task__delete-btn');
-
-  liClone.querySelector('.task__text').textContent = task.text;
+  taskText.textContent = task.text;
   task.isFinished && toggleFinish.classList.add('finished');
+  editButton.addEventListener('click', () => {
+    toggleEditMode(index);
+  });
+  deleteButton.addEventListener('click', () => {
+    deleteTask(index);
+  });
+  todoListContainer.append(liClone);
+};
 
+const createTaskElementEditMode = (task, index) => {
+  const liTemplate = document.querySelector('#task-template-edit-mode');
+  const liClone = document.importNode(liTemplate.content, true);
+  const toggleFinish = liClone.querySelector('.task__toggle-finish');
+  const taskInput = liClone.querySelector('.task__input');
+  const saveButton = liClone.querySelector('.task__save-btn');
+  const cancelButton = liClone.querySelector('.task__cancel-btn');
+  taskInput.value = task.text;
+  task.isFinished && toggleFinish.classList.add('finished');
+  saveButton.addEventListener('click', () => {
+    editTask(newText, index);
+  });
+  cancelButton.addEventListener('click', () => {
+    toggleEditMode(index);
+  });
   todoListContainer.append(liClone);
 };
 
 const createNewTask = () => {
-  const newTaskInput = document.querySelector('.todo-add__text-input');
   const newTask = {
     text: newTaskInput.value,
     isFinished: false,
     editMode: false
   };
-
   newTaskInput.value && todoData.push(newTask);
   newTaskInput.value = '';
+  displayTodoList();
+  pushInLocalStorage();
+};
+
+const toggleEditMode = (index) => {
+  todoData[index].editMode = !todoData[index].editMode;
+  displayTodoList();
+  pushInLocalStorage();
+};
+
+const deleteTask = (index) => {
+  todoData.splice(index, 1);
   displayTodoList();
   pushInLocalStorage();
 };
@@ -45,6 +80,7 @@ const pushInLocalStorage = () => {
 newTaskForm.addEventListener('submit', (event) => {
   event.preventDefault();
   createNewTask();
+  newTaskInput.focus();
 });
 
 displayTodoList();
